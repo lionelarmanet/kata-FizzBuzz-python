71a8b593a2d55a509bc7ba2e5a67b4aab990a586
Lionel Armanet <lionelarmanet@mac.home>
2025-12-18 23:15:07 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# FizzBuzz in python
+
+Bare repo to run a [FizzBuzz](https://codingdojo.org/kata/FizzBuzz/) kata in python.
+
+Tests can be run using this command `ython3 -m pytest -v`.
---
+++ tests/test_sample.py
@@ -0,0 +1,11 @@
+# Write a program that prints the numbers from 1 to 100.
+# But for multiples of three print “Fizz” instead of the number
+# and for the multiples of five print “Buzz”.
+# For numbers which are multiples of both three and five print “FizzBuzz “.
+
+def test_sample():
+    assert 1 + 1 == 2
+
+
+def test_another_sample():
+    assert True is True
