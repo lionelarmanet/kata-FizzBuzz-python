8dca30b0290ce24cabf38150510ac33199585ba6
Antoine Bertin <antoine@bertin.io>
2025-12-19 11:34:19 +0100

    second iteration

--- src/main.py
+++ src/main.py
@@ -1,4 +1,8 @@
 def FizzBuzz(number: int) -> str:
-    if number == 3:
+    if number % 15 == 0:  # perf optimization for mod 3 and mod 5
+        return "FizzBuzz"
+    if number % 3 == 0:
         return "Fizz"
+    if number % 5 == 0:
+        return "Buzz"
     return f"{number}"
--- tests/test_sample.py
+++ tests/test_sample.py
@@ -2,6 +2,7 @@ import pytest
 
 from src.main import FizzBuzz
 
+
 def test_sample():
     assert 1 + 1 == 2
 
@@ -9,12 +10,23 @@ def test_sample():
 def test_another_sample():
     assert True is True
 
+
 @pytest.mark.parametrize(
     "number, expected",
     [
-        (1,"1"),
-        (2,"2"),
-        (3,"Fizz")
-    ])
-def test_fizz_buzz(number:int, expected:str):
+        (1, "1"),
+        (2, "2"),
+        (3, "Fizz"),
+        (4, "4"),
+        (5, "Buzz"),
+        (6, "Fizz"),
+        (7, "7"),
+        (8, "8"),
+        (9, "Fizz"),
+        (10, "Buzz"),
+        (15, "FizzBuzz"),
+    ],
+)
+def test_fizz_buzz(number: int, expected: str):
     assert FizzBuzz(number) == expected
+
